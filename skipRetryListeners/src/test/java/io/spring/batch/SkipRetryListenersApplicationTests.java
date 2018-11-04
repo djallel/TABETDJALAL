c3ad50393eb3d181fa59e735c2fba467d99a44b7
TABET Teejay <b_tabet@esi.dz>
2018-11-04 23:37:16 +0100

    seconde commit

---
+++ skipRetryListeners/src/test/java/io/spring/batch/SkipRetryListenersApplicationTests.java
@@ -0,0 +1,17 @@
+package io.spring.batch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = SkipRetryListenersApplication.class)
+@SpringBootTest
+public class SkipRetryListenersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
